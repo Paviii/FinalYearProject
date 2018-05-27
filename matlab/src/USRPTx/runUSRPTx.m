cccc5c4823bc8e5864eeb18dd7f5547853cb7c53
Pavol Olexa <pavol@dyn3185-165.wlan.ic.ac.uk>
2018-05-27 14:42:47 +0100

    OFDM transmitter

---
+++ matlab/src/USRPTx/runUSRPTx.m
@@ -0,0 +1,35 @@
+%configure parameters
+useCodegen = 1;
+compileIt = 0;
+
+bw = 20e6; %20MHz
+numFrames = 1;
+payloadMessage = 'Andreeeeeeei!!!!';
+numOfPackets = 1000;
+InterpolationFactor = 4;
+%generate waveform
+[ txWaveform, txConfig ] = generateOFDM80211a(numFrames,payloadMessage);
+
+%interpolate
+rateConverter = dsp.FIRRateConverter('InterpolationFactor', 5,... 
+                                         'DecimationFactor', 4); 
+txConv = step(rateConverter,txWaveform);
+txConfig.samplingFreq = bw;% Set desired frequeny
+txConfig.FreqBin = txConfig.samplingFreq/txConfig.FFTLength;% Set frequency bin width
+txConfig.frameLength = txConfig.frameLength*25/20; %resample for N210 transmission. 
+
+% Setup transmitter
+if compileIt
+    disp('Generating code for transmitter');
+    codegen USRPTransmit -args { InterpolationFactor, txConv, numOfPackets }
+end
+
+%transmit 
+% Run transmitter
+if useCodegen
+    USRPTransmit_mex(InterpolationFactor, txConv,numOfPackets)
+else    
+    USRPTransmit(InterpolationFactor, txConv,numOfPackets);
+end
+
+
